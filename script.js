5e70aeecfb639aa9880ab48f9968b54a1b8264a4
monali-28 <113928094+monali-28@users.noreply.github.com>
2024-01-03 22:02:58 +0530

    script.js

--- script.js
+++ script.js
@@ -6,17 +6,27 @@ let arr=Array.from(buttons);
 arr.forEach(button =>{
     button.addEventListener('click',(btn) =>{
         if(btn.target.innerHTML == "="){
-            string=eval(string);
-            inputBox.value=string;
-        }else if(btn.target.innerHTML == 'AC'){
+            try {
+                if ( string.endsWith ("/0") ) {
+                   throw( "Divide by zero error." )
+                } else {
+                    string=eval(string);
+                    inputBox.value=string;
+                }
+             }
+             catch ( e ) {
+                alert("Error: " + e );
+             }
+          }else if(btn.target.innerHTML == 'AC'){
             string="";
             inputBox.value=string;
         }else if(btn.target.innerHTML == 'DEL'){
             string=string.slice(0,-1);
             inputBox.value=string;
-        }else{
+        }
+        else{
             string += btn.target.innerHTML;
-        inputBox.value=string;
+            inputBox.value=string;
         }
         
     })
